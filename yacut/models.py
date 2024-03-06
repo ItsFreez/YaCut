f67984320d01c810cbd454f4d55b2cb9b57b2902
Игорь Митяшин <ItsFreez@yandex.ru>
2024-03-06 12:11:37 +0300

    Update timestamp default

--- yacut/models.py
+++ yacut/models.py
@@ -13,7 +13,7 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(MAX_LEN_ORIGINAL), nullable=False)
     short = db.Column(db.String(MAX_LEN_SHORT), unique=True, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
         return dict(
